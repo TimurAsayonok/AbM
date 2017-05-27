08633929211326c90bf8953f0b03d08ca883e199
Timur Asayonok <t.asaynok@gmail.com>
2017-05-27 15:24:22 +0200

    Changed routes

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import './styles';
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
         <ul className='nav nav-pills'>
-          <li><Link to='/admin'>Admin</Link></li>
-          <li><Link to='/list'>List of Genre</Link></li>
+          <li><Link to='/admin' activeClassName='active'>Admin</Link></li>
+          <li><Link to='/list' activeClassName='active'>List of Genre</Link></li>
         </ul>
         {/* добавили вывод потомков */}
         {this.props.children}
