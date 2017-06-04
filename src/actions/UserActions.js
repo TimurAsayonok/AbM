c6867edcf5fbc7b338096cef6e260c31e12de4fe
Timur Asayonok <t.asaynok@gmail.com>
2017-06-04 23:47:42 +0200

    Added action for User

---
+++ src/actions/UserActions.js
@@ -0,0 +1,18 @@
+import {
+  LOGIN_REQUEST,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS
+} from '../constants/User'
+
+export function login(data) {
+  return {
+    type: LOGIN_SUCCESS
+  }
+}
+
+export function logout() {
+  return {
+    type: LOGOUT_SUCCESS
+  }
+}
