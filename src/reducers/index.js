a406560da00b7226a42512de5f52f090462bdd58
Timur Asayonok <t.asaynok@gmail.com>
2017-06-05 23:51:22 +0200

    Some bug fixed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,4 +3,4 @@ import user from './user'
 
 export const rootReducers = combineReducers({
   user
-})
+});
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
-import { rootReducer } from '../reducers'
+import { createLogger } from 'redux-logger'
+import { rootReducers } from '../reducers'
 
 export default function configureStore() {
   const store = compose(
     applyMiddleware(thunkMiddleware),
     applyMiddleware(createLogger())
-  )(createStore)(rootReducer)
+  )(createStore)(rootReducers)
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers').rootReducer
+      const nextRootReducer = require('../reducers').rootReducers;
       store.replaceReducer(nextRootReducer)
     });
   }
