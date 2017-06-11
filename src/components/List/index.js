2256e4a7cda4a8e328adfe1208a53019b387a9c3
Timur Asayonok <t.asaynok@gmail.com>
2017-06-11 09:12:36 +0200

    Added year buttons on the List page

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
+// import { Link } from 'react-router'
 
 class List extends Component {
+  onYearBtnClick(e) {
+    const { setYear } = this.props;
+    console.log(e.target.innerText);
+    setYear(e.target.innerText);
+  }
+
   render() {
     console.log(this.props);
+    const { year, photos } = this.props.listOfGenre;
+
     return (
-      <div>
-        <div className='row'>
-          <div className='col-md-12'>
-            <h3> Список жанров </h3>
+      <div className='row'>
+        <div className='col-md-12'>
+          <h3> {year} </h3>
+          <div>
+            <button onClick={this.onYearBtnClick.bind(this)}>2016</button>
+            <button onClick={this.onYearBtnClick.bind(this)}>2015</button>
+            <button onClick={this.onYearBtnClick.bind(this)}>2014</button>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col-md-12'>
-            <ul className='list'>
-              <li className='list__item'>
-                <Link to='/genre/house/'>House</Link>
-              </li>
-              <li className='list__item'>
-                <Link to='/genre/dnb/'>Drum and bass</Link>
-              </li>
-              <li className='list__item'>
-                <Link to='/genre/hip-hop/'>Hip-hop</Link>
-              </li>
-            </ul>
-          </div>
+          <p>You have {photos.length} photos </p>
         </div>
       </div>
     )
